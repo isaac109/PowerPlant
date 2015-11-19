98a1e09699490e8f7e5fd1036227da19d9b27b5b
isaac109 <isaac109@mail.chapman.edu>
2015-11-19 00:49:43 -0800

    working on biomes

---
+++ Assets/Assets/AllAssets/scripts/cameraControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraControl : MonoBehaviour {
+
+    float horSpeed = 20f;
+    float verspeed = 20f;
+
+    float cameraDistanceMax = 360f;
+    float cameraDistanceMin = 20f;
+    public float cameraDistance = 100f;
+    float scrollSpeed = 100f;
+
+    public bool canControl = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canControl)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                float h = horSpeed * Input.GetAxis("Mouse X");
+                float v = verspeed * Input.GetAxis("Mouse Y");
+                this.transform.Translate(-h, -v, 0);
+            }
+            cameraDistance += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+            cameraDistance = Mathf.Clamp(cameraDistance, cameraDistanceMin, cameraDistanceMax);
+            this.transform.position = new Vector3(this.transform.position.x, cameraDistance, this.transform.position.z);
+            cameraBounds();
+        }
+	}
+
+    void cameraBounds()
+    {
+        //cxmin = (xcent - xmin) / (cameraDistance - cameraDistanceMin+1);
+    }
+
+}
---
+++ Assets/Assets/AllAssets/scripts/hexManagement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hexManagement : MonoBehaviour {
+
+    public bool show = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+        if (show)
+        {
+            GUILayout.BeginArea(new Rect(10, Screen.height / 2 + 10, 200, Screen.height / 2 + 50));
+            GUILayout.BeginVertical();
+            if (GUI.Button(new Rect(0, 0, 50, 50), "Close"))
+            {
+                this.GetComponent<hexTile2>().closeCameras();
+            }
+            GUILayout.EndVertical();
+            GUILayout.EndArea();
+        }
+    }
+}
